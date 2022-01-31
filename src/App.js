22ad4862dc633704c3a9363d55a9b453602664b3
miketillman95 <matillman95@gmail.com>
2022-01-30 17:45:38 -0700

    fixed jsx runtime, needed latest react.. even though I did npm i react latest

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
-// import Dashboard from "./Dashboard";
-// import Login from "./Login";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
 
 function App() {
-  // const code = new URLSearchParams(window.location.search).get("code");
+  const code = new URLSearchParams(window.location.search).get("code");
 
-  // return <>{code ? <Dashboard code={code} /> : <Login />}</>;
-
-  return <div className="h1">Hello</div>;
+  return <>{code ? <Dashboard code={code} /> : <Login />}</>;
 }
 
 export default App;
---
+++ src/Components/styles/Dashboard.styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const DashBoardContainer = styled.div`
+  padding: 2rem;
+  min-height: 100vh;
+  width: 100%;
+`
+
+export const SearchInput = styled.input`
+  width: 100%;
+  height: 55px;
+  padding: 10px;
+  border-radius: 5px;
+  font-size: 1.5rem;
+
+  &::placeholder {
+    font-size: 1.5rem;
+  }
+`
